a27e89185f66503edfd11fe6a00a5e2168ffa636
Belinda Briggs <belinda@belindaleebriggs.com>
2020-09-28 18:30:37 -0400

    bug: error in app.js getWeather
    - forgot dot before then 
    - extra parenthese after .then's function call
    - updateUI added space between await keyword and request.json()
    - mistyped name of callback fxn in the eventlistener added to the 
    generate button
    -moved getWeather fxn declaration above the event listener that calls it
    
    TODO: Button is clickable but getting error for getWeather

--- website/app.js
+++ website/app.js
@@ -9,24 +9,6 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 let baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
 let apiKey = '&appid=ce49415b2eb93662f798d48a60e01961';
 
-// listen on Generate button to trigger getWeatherData fxn
-document.getElementById('generate').addEventListener('click', getWeatherData);
-
-// Grabs weather data when user clicks Generate button
-function performAction(e){
-  // get data from form
-  const newZip = document.getElementById('zip').value;
-  const feelings = document.getElementById('feelings').value;
-
-  getWeather(baseURL, newZip, apiKey)
-    then(function(data)) {
-      console.log(data);
-      // Add data to data object in server.js via POST request
-      postData('/add',{date:d,temp:data.body.temp,feelings:feelings});
-      updateUI();
-    });
-}
-
 // function to make API call to openweathermap
 const getWeather = async (baseURL, location, key) => {
   const res = await fetch(baseURL + location + key)
@@ -38,6 +20,26 @@ const getWeather = async (baseURL, location, key) => {
           //appropriately handle error
         }
 }
+
+// listen on Generate button to trigger getWeatherData fxn
+document.getElementById('generate').addEventListener('click', getWeather);
+
+// Grabs weather data when user clicks Generate button
+function performAction(e){
+  // get data from form
+  const newZip = document.getElementById('zip').value;
+  const feelings = document.getElementById('feelings').value;
+
+  getWeather(baseURL, newZip, apiKey)
+    .then(function(data) {
+      console.log(data);
+      // Add data to data object in server.js via POST request
+      postData('/add',{date:d,temp:data.body.temp,feelings:feelings});
+      updateUI();
+    });
+}
+
+
 
 // Process a post request
 const postData = async(url='', data = {}) => {
@@ -63,7 +65,7 @@ try {
 const updateUI = async() => {
   const request = await fetch('/all')
   try {
-    const allData = await.request.json()
+    const allData = await request.json()
     document.getElementById('date').innerHTML = `Date: ${allData[0].date}`;
     document.getElementById('temp').innerHTML = `Temp: ${allData[0].temp}`;
     document.getElementById('content').innerHTML = `You are feeling: ${allData[0].feelings}`;
@@ -76,6 +78,8 @@ const updateUI = async() => {
     }
 
 
-// Call getData and postData
+/* For Debugging Call getData and postData
 getData('/all');
 postData('/purple')
+
+End for Debugging */
