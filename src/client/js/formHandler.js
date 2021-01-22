d735b79840ddc3ce8f8871193c41a5248bd62b2a
Belinda Briggs <>
2021-01-22 17:46:43 -0500

    style: fix alignment

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,57 +4,55 @@
 const port = 8081
 
 async function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    // create object to hold form data
-    let formData = {};
+  // create object to hold form data
+  let formData = {};
 
-    // put data from form into an object to pass to API calling fxns
-    formData.destination = document.getElementById('destination').value; 
-    formData.departureDate = document.getElementById('departureDate').value;
+  // put data from form into an object to pass to API calling fxns
+  formData.destination = document.getElementById('destination').value; 
+  formData.departureDate = document.getElementById('departureDate').value;
 
-    console.log(`formData value:` + JSON.stringify(formData));
+  console.log(`formData value:` + JSON.stringify(formData));
     
-  
   // Reset answer area to clear out formatting if entering a new url
-    var results = document.getElementById('destinationResults');
-    results.innerHTML = "Your results will appear here.";
+  var results = document.getElementById('destinationResults');
+  results.innerHTML = "Your results will appear here.";
     
-        console.log('Trying to launch getTripDetails from formHandler!')
-        console.log(`formHandler: Form data entered: Destination - ${formData.destination} and Departure Date - ${formData.departureDate}`)
-        await fetch(`http://localhost:${port}/getTripDetails`, {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers: {
-                'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({formData: formData})
+  console.log('Trying to launch getTripDetails from formHandler!')
+  console.log(`formHandler: Form data entered: Destination - ${formData.destination} and Departure Date - ${formData.departureDate}`)
+  await fetch(`http://localhost:${port}/getTripDetails`, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+           'Content-Type': 'application/json'
+            },
+    body: JSON.stringify({formData: formData})
             },)
-        // Display the data pulled from the APIs 
-        
-        // THIS AREA NEEDS WORK
-            .then(res => res.json()) 
-            .then(function(res) {
-                updateUI(res) 
-            })
-            .catch(err => {
-              console.log(err)
-                document.getElementById('tripErrorMsg').innerHTML = 'Server Error: ' + err;
-            })
-      }
+    
+    // Display the data pulled from the APIs  
+    // THIS AREA NEEDS WORK
+  .then(res => res.json()) 
+  .then(function(res) {
+      updateUI(res) })
+  .catch(err => {
+        console.log(err)
+        document.getElementById('tripErrorMsg').innerHTML = 'Server Error: ' + err;
+         })
+}
 
 
-      function updateUI(data) {  
-        var resultsSection = document.getElementById('resultsSection');
-        resultsSection.className += " " + 'answered';
-        
-        var intro = document.getElementById('destinationResults');
-        var destinationImg = document.getElementById('destinationImg');
-        var weather = document.getElementById('weather');
+function updateUI(data) {  
+  var resultsSection = document.getElementById('resultsSection');
+  resultsSection.className += " " + 'answered';
+      
+  var intro = document.getElementById('destinationResults');
+  var destinationImg = document.getElementById('destinationImg');
+  var weather = document.getElementById('weather');
 
-        intro.innerHTML =`Here is a preview of what you might see on your trip to ${formData.destination}.</br> And a preview of the weather for ${formData.departureDate}.`;
-        destinationImg.innerHTML = `<img class="medium-img" alt="Image of ${formData.destination} from Pixababy" src="${formData.imgURL}">`;
-      }
+  intro.innerHTML =`Here is a preview of what you might see on your trip to ${formData.destination}.</br> And a preview of the weather for ${formData.departureDate}.`;
+  destinationImg.innerHTML = `<img class="medium-img" alt="Image of ${formData.destination} from Pixababy" src="${formData.imgURL}">`;
+}
 
 // listen on Generate button to trigger handleSubmit fxn
 document.getElementById('generate').addEventListener('click', handleSubmit);
