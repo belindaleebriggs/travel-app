d8b16ae263b97886dcc1a85cfa07b1205dd136a0
Belinda Briggs <>
2021-01-23 15:17:19 -0500

    feat: UI grid layout update

--- src/client/index.js
+++ src/client/index.js
@@ -1,18 +1,11 @@
 import { handleSubmit } from './js/formHandler'
-// import { postData } from './js/formHandler'
 import { updateUI } from './js/formHandler'
 
 
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/layout.scss';
-import './styles/footer.scss';
 import './styles/form.scss';
-import './styles/header.scss';
-
-/* IMAGE IMPORT NEEDS TROUBLESHOOTING
-import './media/img/BLB_side.png'
-*/
 
 export { 
     handleSubmit,
